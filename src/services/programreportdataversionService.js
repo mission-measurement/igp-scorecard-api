ef44b30aba0cc94156ecff109eb1c3ca3339d601
remoroethlisberger <remo.roethlisberger@students.unibe.ch>
2020-06-10 00:19:20 +0200

    Fix error with scorecard not updating

--- src/services/programreportdataversionService.js
+++ src/services/programreportdataversionService.js
@@ -2,7 +2,7 @@ const db = require('../database/databases');
 const SQL = require('sql-template-strings');
 
 const programreportdataversions = async (programreportid) => {
-  const q0 = SQL`SELECT programreportdataversionid, ispublished, isinitial, comments FROM programreportdataversions WHERE programreportid = ${programreportid} ORDER BY programreportdataversionid LIMIT 1`;
+  const q0 = SQL`SELECT programreportdataversionid, ispublished, isinitial, comments FROM programreportdataversions WHERE programreportid = ${programreportid} ORDER BY programreportdataversionid DESC LIMIT 1`;
   const r = await db.query(q0);
 
   if (r.length == 1) {
