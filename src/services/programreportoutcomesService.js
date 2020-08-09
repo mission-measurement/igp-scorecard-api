92ee93575593e474f42477e2b0add7e39067c1b8
remoroethlisberger <remo.roethlisberger@students.unibe.ch>
2020-08-10 00:16:32 +0200

    Select primaryoutcome only once

--- src/services/programreportoutcomesService.js
+++ src/services/programreportoutcomesService.js
@@ -15,14 +15,14 @@ const programreportoutcomes = async (
   programreportid,
   programreportdataversionid
 ) => {
-  const q0 = SQL`SELECT * FROM programreportoutcomes AS pro 
+  const q0 = SQL`SELECT programreportdataversionid, programreportid, pro.outcomeid AS outcomeid, outcomerank, MAX(isprimary) AS isprimary, measured, acheived, pro.beneficiarytypeid, oq.*, o.*, g.*, i.*, b.* FROM programreportoutcomes AS pro 
   LEFT JOIN (SELECT name AS outcomename, outcomeid, genomeid, impactareaid, description, definition FROM taxonomy_db.outcomes) AS o ON pro.outcomeid = o.outcomeid
   LEFT JOIN taxonomy_db.outcomequalifier AS oq ON (pro.outcomeid = oq.outcomeid) 
   LEFT JOIN (SELECT name AS genomename, genomeid FROM taxonomy_db.genomes) AS g ON g.genomeid = o.genomeid
   LEFT JOIN  (SELECT name AS impactareaname, impactareaid FROM taxonomy_db.impactareas) AS i ON i.impactareaid = o.impactareaid
   LEFT JOIN (SELECT name AS beneficiaryname, beneficiarytypeid FROM taxonomy_db.beneficiarytypes) AS b USING (beneficiarytypeid)
   /* might need to filter out old revisions */
-  WHERE programreportdataversionid = ${programreportdataversionid} AND programreportid = ${programreportid} 
+  WHERE programreportdataversionid = ${programreportdataversionid} AND programreportid = ${programreportid} GROUP BY pro.outcomeid
   `;
 
   const result = await db.query(q0);
