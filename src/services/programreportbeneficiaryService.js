e93820b2517c992219642f9a9c9a68918f232155
remoroethlisberger <remo.roethlisberger@students.unibe.ch>
2020-08-18 23:14:05 +0200

    Find local max of beneficiary reported data

--- src/services/programreportbeneficiaryService.js
+++ src/services/programreportbeneficiaryService.js
@@ -11,7 +11,7 @@ const programreportbeneficiaries = async (
   const add_char = 'Additional Characteristics';
   // probably want to add the AND clause:  AND prb.programreportdataversionid = ${programreportdataversionid}
   // back sometime later
-  const q0 = SQL`SELECT prb.inputvalue AS value, si.labeltext AS beneficiarychar, CASE WHEN si.questionid = 42 THEN ${gender} WHEN si.questionid = 43 THEN ${age} WHEN si.questionid = 44 THEN ${ethnicity} WHEN si.questionid = 45 THEN ${add_char} END AS category FROM programreportbeneficiaries AS prb INNER JOIN (SELECT * FROM survey_db.inputvalues WHERE questionid IN (42, 43, 44, 45)) AS si ON prb.inputdataid = si.inputvalueid AND prb.programreportid = ${programreportid} ORDER BY \`order\``;
+  const q0 = SQL`SELECT prb.inputvalue AS value, si.labeltext AS beneficiarychar, CASE WHEN si.questionid = 42 THEN ${gender} WHEN si.questionid = 43 THEN ${age} WHEN si.questionid = 44 THEN ${ethnicity} WHEN si.questionid = 45 THEN ${add_char} END AS category FROM programreportbeneficiaries AS prb INNER JOIN (SELECT * FROM survey_db.inputvalues WHERE questionid IN (42, 43, 44, 45)) AS si ON prb.inputdataid = si.inputvalueid AND prb.programreportid = ${programreportid} AND prb.programreportdataversionid = (SELECT MAX(programreportdataversionid) FROM igp_apps_db.programreportbeneficiaries WHERE programreportid = ${programreportid}) ORDER BY \`order\``;
   const r = await db.query(q0);
   let chars = [];
   for (i = 0; i < r.length; i++) {
