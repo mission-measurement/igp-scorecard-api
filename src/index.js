3d85742926ef3410c7ee1fc09d949b6e2cc0b515
Tony Muhm <52762711+mmtmuhm@users.noreply.github.com>
2021-01-12 18:00:54 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,33 @@ const logger = require('morgan');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
+
+var openPaths = ["/scorecard/healthy"];
+var authCheck = (req, res, next) => {
+  var path = req._parsedUrl.pathname;
+  if (openPaths.includes(path)) {
+    next();
+  } else {
+    auth.authenticateToken(req, res, next);
+  }
+};
+
+
+app.get("/scorecard/healthy", (req, res) => {
+    res.status(200).send("Healthy!");
+  });
+
+
 app.use(express.json());
 app.use(cors());
-app.use(logger('dev'));
+//app.use(logger('dev'));
+app.use(
+    logger('combined', {
+      skip: function(req, res) {
+        return res.statusCode < 400;
+      }
+    })
+  );
 
 // public assets (html, css, img)
 app.use('/public', express.static(path.join(__dirname, '../public')));
