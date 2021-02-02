aac34c24faea2b47ff79306deabb84c86b62dabc
remoroethlisberger <remo.r@gmx.ch>
2021-02-02 05:27:23 +0100

    Making outcomecriteria smaller and indent more

--- src/services/programreportoutcomesService.js
+++ src/services/programreportoutcomesService.js
@@ -22,31 +22,37 @@ const programreportoutcomes = async (
   LEFT JOIN  (SELECT name AS impactareaname, impactareaid FROM taxonomy_db.impactareas) AS i ON i.impactareaid = o.impactareaid
   LEFT JOIN (SELECT name AS beneficiaryname, beneficiarytypeid FROM taxonomy_db.beneficiarytypes) AS b USING (beneficiarytypeid)
   /* might need to filter out old revisions */
-  WHERE programreportdataversionid = ${programreportdataversionid} AND programreportid = ${programreportid} GROUP BY pro.outcomeid
+  WHERE programreportdataversionid = ${programreportdataversionid} AND programreportid = ${programreportid} GROUP BY pro.outcomeid ORDER BY pro.outcomerank DESC
   `;
 
   const result = await db.query(q0);
   let outcomes = [];
   let primaryoutcome = {};
   for (i = 0; i < result.length; i++) {
-    let outcome = {};
-    outcome.id = result[i].outcomeid;
-    outcome.name = result[i].outcomename;
-    outcome.genome = result[i].genomename;
-    outcome.impactarea = result[i].impactareaname;
-    outcome.beneficiary = result[i].beneficiaryname;
-    outcome.description = result[i].description;
-    outcome.definition = result[i].definition;
-    outcome.qualifier = result[i].qualifier;
-    outcome.criterias = await outcomecriterias(outcome.id);
-    outcome.outcomerank = result[i].outcomerank;
-    outcome.isprimary = result[i].isprimary;
-    outcome.measured = result[i].measured;
-    outcome.acheived = result[i].acheived;
     if (outcome.isprimary) {
       primaryoutcome = outcome;
+      continue;
+    } else {
+      if (outcomes.length < 5) {
+        let outcome = {};
+        outcome.id = result[i].outcomeid;
+        outcome.name = result[i].outcomename;
+        outcome.genome = result[i].genomename;
+        outcome.impactarea = result[i].impactareaname;
+        outcome.beneficiary = result[i].beneficiaryname;
+        outcome.description = result[i].description;
+        outcome.definition = result[i].definition;
+        outcome.qualifier = result[i].qualifier;
+        outcome.criterias = await outcomecriterias(outcome.id);
+        outcome.outcomerank = result[i].outcomerank;
+        outcome.isprimary = result[i].isprimary;
+        outcome.measured = result[i].measured;
+        outcome.acheived = result[i].acheived;
+        outcomes.push(outcome);
+      } else {
+        break;
+      }
     }
-    outcomes.push(outcome);
   }
   return {
     outcomes: outcomes,
