03742ed47caf7ba448fc86298784c4ee71541aff
remoroethlisberger <remo.r@gmx.ch>
2021-03-05 11:53:17 +0100

    Add all of the states

--- src/services/programlocationService.js
+++ src/services/programlocationService.js
@@ -60,6 +60,8 @@ const getLocation = async (
               country.regions.map((region) => region.name).join(', ') +
               ')'
             );
+          } else if (country.regions.length == 50) {
+            return ' (all of the states)';
           } else {
             return (
               country.name +
