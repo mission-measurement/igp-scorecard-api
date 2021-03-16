d046453e98122b7713e3abe889898dd2c8b2f7a7
remoroethlisberger <remo.r@gmx.ch>
2021-03-16 18:11:52 +0100

    Adding ethnicity from international programs

--- src/services/programreportbeneficiaryService.js
+++ src/services/programreportbeneficiaryService.js
@@ -13,19 +13,39 @@ const programreportbeneficiaries = async (
   // back sometime later
   const q0 = SQL`SELECT prb.inputvalue AS value, si.labeltext AS beneficiarychar, CASE WHEN si.questionid = 42 THEN ${gender} WHEN si.questionid = 43 THEN ${age} WHEN si.questionid = 44 THEN ${ethnicity} WHEN si.questionid = 45 THEN ${add_char} END AS category FROM programreportbeneficiaries AS prb INNER JOIN (SELECT * FROM survey_db.inputvalues WHERE questionid IN (42, 43, 44, 45)) AS si ON prb.inputdataid = si.inputvalueid AND prb.programreportdataversionid = ${programreportdataversionid} ORDER BY \`order\``;
   const r = await db.query(q0);
+  const q1 = SQL`SELECT value, category AS beneficiarychar, CASE WHEN questionname = 'agerange' THEN ${age} WHEN questionname = 'raceethnicity' THEN ${ethnicity} END AS category FROM additionalmoodysquestions WHERE questionname IN ('agerange', 'raceethnicity') AND value IS NOT NULL AND programreportdataversionid = ${programreportdataversionid} ORDER BY questionname`;
+  const s = await db.query(q1);
+
   let chars = [];
-  for (i = 0; i < r.length; i++) {
-    let char = {};
-    char.beneficiarychar = r[i].beneficiarychar;
-    char.value = r[i].value;
-    char.category = r[i].category;
+  if (r.length > 0) {
+    for (let i = 0; i < r.length; i++) {
+      let char = {};
+      char.beneficiarychar = r[i].beneficiarychar;
+      char.value = r[i].value;
+      char.category = r[i].category;
+
+      if (
+        parseInt(char.value) >= 10 ||
+        char.beneficiarychar == gender ||
+        char.beneficiarychar == ethnicity
+      ) {
+        chars.push(char);
+      }
+    }
 
     if (
-      parseInt(char.value) >= 10 ||
-      char.beneficiarychar == gender ||
-      char.beneficiarychar == ethnicity
+      !chars.filter((char) => {
+        return char.category == ethnicity;
+      }).length &&
+      s.length > 0
     ) {
-      chars.push(char);
+      for (let i = 0; i < s.length; i++) {
+        let char = {};
+        char.beneficiarychar = s[i].beneficiarychar;
+        char.value = s[i].value;
+        char.category = s[i].category;
+        chars.push(char);
+      }
     }
   }
 
