0f007c792635d75c9e521026f0276ade326b9111
remoroethlisberger <remo.r@gmx.ch>
2021-04-01 07:22:08 +0200

    Change filename to fil

--- src/helpers/uploadScorecard.js
+++ src/helpers/uploadScorecard.js
@@ -3,21 +3,21 @@ const axios = require('axios');
 const FormData = require('form-data');
 
 const uploadScorecard = async (programreportid, filename) => {
-    let data = new FormData();
-    data.append('scorecard', fs.createReadStream(filename));
-    let config = {
-        method: 'post',
-        url: `${process.env.API_HOST}/aws/programreports/scorecard?programreportid=${programreportid}`,
-        headers: {
-            Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
-            ...data.getHeaders(),
-        },
-        data: data,
-    };
-    let r = await axios(config);
-    return r.data; // returns obj with prop location, filename and programreportuuid
+  let data = new FormData();
+  data.append('scorecard', fs.createReadStream(filename));
+  let config = {
+    method: 'file',
+    url: `${process.env.API_HOST}/aws/programreports/scorecard?programreportid=${programreportid}`,
+    headers: {
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+      ...data.getHeaders(),
+    },
+    data: data,
+  };
+  let r = await axios(config);
+  return r.data; // returns obj with prop location, filename and programreportuuid
 };
 
 module.exports = {
-    uploadScorecard
-}
+  uploadScorecard,
+};
